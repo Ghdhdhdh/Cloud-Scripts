f4c8ab8b29068c7d343798b55c3dc98b2ca23266
Jacob_str <80145275+Ghdhdhdh@users.noreply.github.com>
2023-06-04 00:39:23 +0000

    Add Vnc #1

---
+++ VncSetupUbuntu.sh
@@ -0,0 +1,12 @@
+# Install Packages
+sudo apt update
+sudo apt install ubuntu-desktop
+sudo apt install tightvncserver
+sudo apt install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
+
+# Make Default Config File
+vncserver :1
+
+# Edit Config File\
+curl -L
+# ~/.vnc/xstartup
